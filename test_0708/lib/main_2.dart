ef65bd3c3406649dc8466efa54c068038f10794d
DEVELOPER D <silver_liq9118@naver.com>
2024-07-09 23:31:56 +0900

    Update main_2.dart

--- test_0708/lib/main_2.dart
+++ test_0708/lib/main_2.dart
@@ -1,22 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-
   runApp(MaterialApp(
-      home:Scaffold(
-        appBar: AppBar(
-            title: Text(
-                'Study to Container'
-            )
-        ),
-        body: CustomContainer(),
-      )
-
-
-  )
-
-  );
-
+      home: Scaffold(
+    appBar: AppBar(title: Text('Study to Container')),
+    body: CustomContainer(),
+  )));
 }
 
 class CustomContainer extends StatelessWidget {
@@ -24,10 +13,23 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Hello Container'),
+    return Center(
+      child: Container(
+        width: 100,
+        height: double.infinity,
+
+        padding: EdgeInsets.fromLTRB(10, 12, 12, 12),
+        //margin: EdgeInsets.symmetric(vertical:12 ,horizontal:30 ),
+        decoration: BoxDecoration(
+          color: Colors.blueAccent.shade100,
+          border: Border.all(color: Colors.red, width: 5, style: BorderStyle.solid),
+          borderRadius: BorderRadius.circular(10),
+          boxShadow: [
+            BoxShadow(color: Colors.grey, offset: Offset(6,6), blurRadius: 10, spreadRadius: 10),
+          ]
+        ),
+        child: Center(child: Container(child: Text('Hello Container'), color: Colors.yellow,)),
+      ),
     );
   }
 }
-
-
