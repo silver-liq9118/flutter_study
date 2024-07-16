0dae0f7c60f76e68bfd189fef93dc3a50e87b605
DEVELOPER D <silver_liq9118@naver.com>
2024-07-16 22:52:56 +0900

    Create main_8(Gesture).dart

---
+++ test_0708/lib/main_8(Gesture).dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: SafeArea(
+      child: Scaffold(
+        body: Body(),
+      ),
+    ),
+  ));
+}
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        TestCheckBox(),
+        TestCheckBox(),
+        TestCheckBox(),
+
+        
+      ],
+
+    );
+  }
+}
+
+class TestCheckBox extends StatefulWidget {
+  const TestCheckBox({super.key});
+
+  @override
+  State<TestCheckBox> createState() => _TestCheckBoxState();
+}
+
+class _TestCheckBoxState extends State<TestCheckBox> {
+
+  late List<bool> values ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    values = [false,false,false];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(value : values[0], onChanged: (value) => changeValue(0,value:value)),
+        Checkbox(value: values[1], onChanged: (value) => changeValue(1,value:value)),
+        Checkbox(value: values[2], onChanged: (value) => changeValue(2,value:value)),
+      ],
+    );
+  }
+
+
+  void changeValue(int index, {bool? value=false}) {
+    setState((){
+      values[index] = value!;
+
+    });
+
+  }
+}
