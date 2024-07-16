38fdd01b9ffb19f04bad9661668e97380d0118a3
DEVELOPER D <silver_liq9118@naver.com>
2024-07-16 23:08:56 +0900

    Update main_8(Gesture).dart

--- test_0708/lib/main_8(Gesture).dart
+++ test_0708/lib/main_8(Gesture).dart
@@ -21,8 +21,8 @@ class Body extends StatelessWidget {
         TestCheckBox(),
         TestCheckBox(),
         TestCheckBox(),
+        TestRadioButton(),
       ],
-
     );
   }
 }
@@ -35,33 +35,72 @@ class TestCheckBox extends StatefulWidget {
 }
 
 class _TestCheckBoxState extends State<TestCheckBox> {
-
-  late List<bool> values ;
+  late List<bool> values;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    values = [false,false,false];
+    values = [false, false, false];
   }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Checkbox(value : values[0], onChanged: (value) => changeValue(0,value:value)),
-        Checkbox(value: values[1], onChanged: (value) => changeValue(1,value:value)),
-        Checkbox(value: values[2], onChanged: (value) => changeValue(2,value:value)),
+        Checkbox(
+            value: values[0],
+            onChanged: (value) => changeValue(0, value: value)),
+        Checkbox(
+            value: values[1],
+            onChanged: (value) => changeValue(1, value: value)),
+        Checkbox(
+            value: values[2],
+            onChanged: (value) => changeValue(2, value: value)),
       ],
     );
   }
 
-
-  void changeValue(int index, {bool? value=false}) {
-    setState((){
+  void changeValue(int index, {bool? value = false}) {
+    setState(() {
       values[index] = value!;
-
     });
+  }
+} // 체크박스
 
+class TestRadioButton extends StatefulWidget {
+  const TestRadioButton({super.key});
+
+  @override
+  State<TestRadioButton> createState() => _TestRadioButtonState();
+}
+
+enum TestRadioValue {
+  test1,
+  test2,
+  test3,
+}
+
+class _TestRadioButtonState extends State<TestRadioButton> {
+  TestRadioValue? selectValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Radio<TestRadioValue>(
+            value: TestRadioValue.test1,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!)),
+        Radio<TestRadioValue>(
+            value: TestRadioValue.test2,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!)),
+        Radio<TestRadioValue>(
+            value: TestRadioValue.test3,
+            groupValue: selectValue,
+            onChanged: (value) => setState(() => selectValue = value!)),
+      ],
+    );
   }
 }
